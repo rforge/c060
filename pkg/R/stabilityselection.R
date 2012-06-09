bfd1704b23ec20d902eac701bcec76caba5e9e05
msill <msill@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-06-09 19:11:38 +0000

    R and man folder + DESCRITPION file
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/c060@3 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/stabilityselection.R
+++ pkg/R/stabilityselection.R
@@ -1,6 +1,3 @@
-library(glmnet)
-library(parallel)
-
 stability.path <- function(y,x,mc.cores=getOption("mc.cores", 2L),size=0.632,steps=100,weakness=1,...){
 	fit <- glmnet(x,y,...)
 	p <- ncol(x)
@@ -51,76 +48,13 @@ plot.stabpath <- function(stabpath,fwer,pi_thr=0.8,...){
 	cols <- rep("black",p)
 	cols[sel$stable] <- "red"
 	par(mfrow=c(2,1))
-	matplot(t(stabpath$fit$beta),xaxt="n",type="l",col=cols,lty=1,ylab=expression(paste(beta[i]))
+	matplot(t(as.matrix(stabpath$fit$beta)),xaxt="n",type="l",col=cols,lty=1,ylab=expression(paste	(beta[i]))
         ,xlab=expression(paste(lambda)),main="Penalization Path",cex.lab=1,cex.axis=1,...)
-	matplot(t(stabpath$stabpath),xaxt="n",type="l",col=cols,lty=1,ylab=expression(paste(hat(Pi)))
+	matplot(t(as.matrix(stabpath$stabpath)),xaxt="n",type="l",col=cols,lty=1,ylab=expression(paste(hat(Pi)))
         ,xlab=expression(paste(lambda)),main="Stability Path",ylim=c(0,1),cex.lab=1,cex.axis=1,...)
 	abline(h=pi_thr,col="darkred",lwd=1,lty=1)
 	abline(v=sel$lpos,col="darkred",lwd=1,lty=1)
 	#text(x=20,y=0.9,paste(expression(paste(lambda)),"=",paste(round(sel[[2]],digits=3)),sep=""),cex=0.75)
 	return(sel)
 }
-
-#gaussian
-x=matrix(rnorm(100*10000),100,10000)
-y=rnorm(100)
-#1 core
-system.time(res <- stability.path(y,x,weakness=1,mc.cores=1))
-#2 cores
-system.time(res <- stability.path(y,x,weakness=1,mc.cores=2))
-# additional arguments for glmnet nlambda alpha 
-system.time(res <- stability.path(y,x,weakness=1,mc.cores=2,nlambda=50,alpha=0.5))
-stability.selection(res,fwer=0.2)
-plot.stabpath(res,fwer=0.2)
-
-#binomial
-y=sample(1:2,100,replace=TRUE)
-res <- stability.path(y,x,weakness=1,mc.cores=2,family="binomial")
-stability.selection(res,fwer=0.2)
-plot.stabpath(res,fwer=0.2)
-    
-#multinomial
-#y=sample(1:4,100,replace=TRUE)
-#res <- stability.path(y,x,weakness=0.2,family="multinomial")
-#stability.selection(res,fwer=0.2)
-#plot.stabpath(res,fwer=0.2)
-    
-#poisson
-N=100; p=10000
-nzc=5
-x=matrix(rnorm(N*p),N,p)
-beta=rnorm(nzc)
-f = x[,seq(nzc)]%*%beta
-mu=exp(f)
-y=rpois(N,mu)
-res <- stability.path(y,x,weakness=1,mc.cores=2
-                      ,family="poisson")
-stability.selection(res,fwer=0.2)
-plot.stabpath(res,fwer=0.2)
-
-#Cox
-library(survival)
-set.seed(10101)
-N=100;p=10000
-nzc=p/3
-x=matrix(rnorm(N*p),N,p)
-beta=rnorm(nzc)
-fx=x[,seq(nzc)]%*%beta/3
-hx=exp(fx)
-ty=rexp(N,hx)
-tcens=rbinom(n=N,prob=.3,size=1)# censoring indicator
-y=cbind(time=ty,status=1-tcens) # y=Surv(ty,1-tcens) with library(survival)
-
-res <- stability.path(y,x,weakness=1,mc.cores=2,family="cox")
-stability.selection(res,fwer=0.2)
-plot.stabpath(res,fwer=0.2)
-
-
-
-
-
-
-
-
-
 
