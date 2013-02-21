c69aa0dce34122f6fa3ce4f20abde0af9b6d9fac
msill <msill@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-02-21 08:55:50 +0000

    natalias änderungen
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/c060@100 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/R/plot_points_inter_search.R
@@ -0,0 +1,10 @@
+
+###########################################################################################################
+plot.points.int.search<-function(summary.int, start.N=21){
+  # plot visited points vs. iteration steps
+  niter<-nrow(summary.int$info) - start.N
+  iter<-c(rep(0,start.N), c(1:niter))
+  plot(summary.int$info$alpha, iter, xlab=expression(alpha), ylab="Iteration", pch=20)
+  grid(NA, niter+1, lwd=2)
+  abline(v=summary.int$opt.alpha, col="red")
+}
